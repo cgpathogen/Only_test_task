f180ec4a2c542be71080ed12d923801e55d2e081
anatoly_bobrov <acrocanthbm@gmail.com>
2025-04-29 22:26:07 +0300

    Теперь точно финал

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-import time
-
 from AnotherPage import Another_page
 from conftest import init_driver
 from BasePage import BasePage
@@ -7,13 +5,13 @@ from MainPage import Main_page
 
 
 def test_main(init_driver):
-    driver = init_driver
-    base = BasePage(driver) # создаём экземпляр класса страницы с базовыми методами
-    main = Main_page(driver) # создаём экземпляр класса главной страницы
-    another = Another_page(driver) # создаём экземпляр класса третьей страницы
+    driver = init_driver # фикстура с хромдрайвером
+    base = BasePage(driver) # экземпляр класса страницы с базовыми методами
+    main = Main_page(driver) # экземпляр класса главной страницы
+    another = Another_page(driver) # экземпляр класса третьей страницы
 
     base.go_to_main_url()
-    base.click_accept_cookie_btn() # принимаем куки, чтобы gjg-fg не мешал нам видеть, как мы наводим курсор на кнопку в методе hover_start_project_btn :))
+    base.click_accept_cookie_btn() # принимаем куки, чтобы поп-ап не мешал нам видеть, как мы наводим курсор на кнопку в методе hover_start_project_btn :))
     base.scroll_to_footer()
     main.hover_start_project_btn()
     main.print_footer_text()
